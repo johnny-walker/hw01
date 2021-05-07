52be79b9a979edf047ef6593efa38cfae4ef5d8e
Walker Hsu <40268687+johnny-walker@users.noreply.github.com>
2021-05-07 16:41:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//https://www.tutorialsteacher.com/nodejs/expressjs-web-application (this webapp's step-by-step tutorial)
+//https://www.tutorialsteacher.com/nodejs/expressjs-web-application (webapp's step-by-step tutorial)
 //https://www.tutorialspoint.com/http/http_methods.htm  (HTTP request methods)
 //https://www.tutorialspoint.com/http/http_status_codes.htm (HTTP response status codes)
 
